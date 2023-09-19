33895efc44aca33c4352bad018ce4093045327bb
RobinTTY <muellerobin95@gmail.com>
2023-09-19 21:00:35 +0200

    Improve section-separator responsiveness

--- components/section-separator.ts
+++ components/section-separator.ts
@@ -9,7 +9,7 @@ export class SectionSeparator extends LitElement {
       content: '';
       display: inline-block;
       position: relative;
-      width: min(20vw, 300px);
+
       height: 1px;
       top: -0.35em;
       background-color: rgb(168, 178, 209);
@@ -19,9 +19,32 @@ export class SectionSeparator extends LitElement {
       padding: 0 1em;
     }
 
+    @media (min-width: 769px) {
+      :host::before,
+      :host::after {
+        width: min(25vw, 300px);
+      }
+    }
+
     @media (max-width: 768px) {
+      :host::before,
+      :host::after {
+        width: 20vw;
+      }
+
       span {
         font-size: 0.85em;
+      }
+    }
+
+    @media (max-width: 450px) {
+      :host::before,
+      :host::after {
+        width: 15vw;
+      }
+
+      span {
+        font-size: 0.75em;
       }
     }
   `;
